ac21da698a276e1820257d1399a4c29e62295845
irfan-fadhlurrahman <irfanfadh43@gmail.com>
2022-05-24 10:53:11 +0700

    add scripts

---
+++ pre-requisites/predict.py
@@ -0,0 +1,30 @@
+import dill
+import pandas as pd
+
+from flask import Flask
+from flask import request
+from flask import jsonify
+
+model_file = 'logreg-v2.bin'
+with open(f"./model/{model_file}", 'rb') as file_in:
+     model = dill.load(file_in)
+
+app = Flask('churn')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    customer = request.get_json()
+    
+    X = pd.DataFrame([customer])
+    y_proba = model.predict_proba(X)[0, 1]
+    churn = y_proba >= 0.5
+    
+    result = {
+        "churn_probability": float(y_proba),
+        "churn": bool(churn)
+    }
+
+    return jsonify(result)
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=9696)
